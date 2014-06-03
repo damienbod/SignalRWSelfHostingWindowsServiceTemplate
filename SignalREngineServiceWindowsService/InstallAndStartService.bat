e01c121585dfc1ce973c9a52278dcd9bd870edb5
damienbod <damien_bod@hotmail.com>
2014-06-03 21:43:54 +0200

    init

--- SignalREngineServiceWindowsService/InstallAndStartService.bat
+++ SignalREngineServiceWindowsService/InstallAndStartService.bat
@@ -1,3 +1,3 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil /LogToConsole=true "C:\git\SignalRWindowsServiceExample\SignalREngineServiceWindowsService\bin\Release\SignalREngineServiceWindowsService.exe"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil /LogToConsole=true "C:\git\damienbod\SignalRWSelfHostingWindowsServiceTemplate\SignalREngineServiceWindowsService\bin\Release\SignalREngineServiceWindowsService.exe"
 NET START "SignalREngineServiceWindowsService"
 pause
--- SignalREngineServiceWindowsService/UnInstallService.bat
+++ SignalREngineServiceWindowsService/UnInstallService.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil /LogToConsole=true /u "C:\git\SignalRWindowsServiceExample\SignalREngineServiceWindowsService\bin\Release\SignalREngineServiceWindowsService.exe"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil /LogToConsole=true /u "C:\git\damienbod\SignalRWSelfHostingWindowsServiceTemplate\SignalREngineServiceWindowsService\bin\Release\SignalREngineServiceWindowsService.exe"
 pause
